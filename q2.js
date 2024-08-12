3dd96b99557aec6d2173f39989f9a3e548bb6d8c
26122004harshit <132337971+26122004harshit@users.noreply.github.com>
2024-08-12 15:42:23 +0530

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,5 @@
+console.log(multiplyNumber(3,4));
+
+const multiplyNumber = function (a,b) {
+    return a*b;
+}
---
+++ q4.js
@@ -0,0 +1,12 @@
+{console.log(x);
+console.log(y);
+console.log(z);
+
+var x = 10;
+let y = 20;
+const z = 30;
+
+console.log(x);
+console.log(y);
+console.log(z);
+}
